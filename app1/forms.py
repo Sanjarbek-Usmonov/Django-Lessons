07c9fc86e28dcb8ce89ecb69fa1b448f0ad3f2d4
Sanjarbek Usmonov <sanjarbekusmonov13@gmail.com>
2022-09-24 19:15:21 +0500

    Updated and added validators and clean function in forms

--- app1/forms.py
+++ app1/forms.py
@@ -1,12 +1,28 @@
 from dataclasses import fields
 from django import forms
-
 from app1.models import Info
+
 
 class InfoForm(forms.Form):
     name = forms.CharField(max_length=100)
     mobile = forms.CharField(max_length=150)
-    email = forms.EmailField()
+    email = forms.CharField()
+
+    def clean_mobile(self):
+        number = self.cleaned_data['mobile']
+        qs = Info.objects.filter(mobile=number)
+        if qs:
+            raise forms.ValidationError('Bu mobil raqam oldin kiritilgan!')
+        return number
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        qs = Info.objects.filter(email=email)
+        if qs:
+            raise forms.ValidationError('Bu email oldin kiritilgan!')
+        elif '@' not in email or '.' not in email:
+            raise forms.ValidationError('Email noto\'g\'ri kiritildi!')
+        return email
 
 class InfoModelForm(forms.ModelForm):
     class Meta:
--- app1/views.py
+++ app1/views.py
@@ -4,20 +4,20 @@ from .models import *
 from .forms import InfoForm, InfoModelForm
 
 def index(request):
-    form = InfoModelForm()
+    form = InfoForm()
     if request.method == 'POST':
-        form = InfoModelForm(request.POST)
+        form = InfoForm(request.POST)
         if form.is_valid():
-            # ins = Info(
-            #     name = form.cleaned_data['name'],
-            #     mobile = form.cleaned_data['mobile'],
-            #     email = form.cleaned_data['email'],
-            # )
-            # ins.save()
-            form.save()
+            ins = Info(
+                name = form.cleaned_data['name'],
+                mobile = form.cleaned_data['mobile'],
+                email = form.cleaned_data['email'],
+            )
+            ins.save()
+            # form.save()
             return render(request, 'index.html')
         
     else: 
-        form = InfoModelForm()
+        form = InfoForm()
     return render(request, 'index.html', {'form': form})
 
